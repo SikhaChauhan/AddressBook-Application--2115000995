ac0a8632c484269b6a8ba94e951871707d253edf
SikhaChauhan <csikha1010@gmail.com>
2025-03-16 19:52:43 +0530

    [Sikha] Add: Added all CRUD Methods for Address Book Entries

---
+++ BusinessLayer/Interface/IAddressBookBL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModelLayer.Model;
+using RepositoryLayer.Service;
+
+namespace BusinessLayer.Interface
+{
+    public interface IAddressBookBL
+    {
+        Task<IEnumerable<AddressBookEntity>> GetAllContacts();
+        Task<AddressBookEntity?> GetContactById(int id);
+        Task<AddressBookEntity> AddContact(AddressBookEntity contact);
+        Task<AddressBookEntity?> UpdateContact(int id, AddressBookEntity contact);
+        Task<bool> DeleteContact(int id);
+    }
+}

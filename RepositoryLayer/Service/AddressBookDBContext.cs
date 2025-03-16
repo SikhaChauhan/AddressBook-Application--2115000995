e65287eb680d602087055517c9f8250b0ede0adc
SikhaChauhan <csikha1010@gmail.com>
2025-03-16 18:45:41 +0530

    [Sikha] Add: initial setup of DbContext is done for implementation purpose

---
+++ RepositoryLayer/Service/AddressBookDBContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ModelLayer.Model;
+
+namespace RepositoryLayer.Service
+{
+    public class AddressBookDBContext : DbContext
+    {
+        public AddressBookDBContext(DbContextOptions<AddressBookDBContext> options) : base(options) { }
+
+        public DbSet<UserEntity> Users { get; set; }
+        public DbSet<AddressBookEntity> AddressBookEntries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserEntity>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+        }
+    }
+}
